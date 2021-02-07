86b7ca0b2c21fb2495fa2b824eb53babc3c37b1a
rpauli <robin.pauli@posteo.de>
2021-02-07 16:12:58 +0100

    add ressource to routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\QuestionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/questions', function () {
-    return view('questions');
-});
+Route::resource('questions', QuestionController::class);
