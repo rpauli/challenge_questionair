d7079acc3db3a773e1c6a6bbb0edf510871c8db3
rpauli <robin.pauli@posteo.de>
2021-02-07 16:11:49 +0100

    add controller for questions

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Question::latest()->paginate(5);
+    
+        return view('questions.index',compact('data'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+        }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('questions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'text' => 'required',
+
+        ]);
+    
+        Question::create($request->all());
+     
+        return redirect()->route('questions.index')
+                        ->with('success','Question created successfully.');
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return view('questions.show',compact('question'));
+        }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        return view('questions.edit',compact('question'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        $request->validate([
+            'text' => 'required',
+        ]);
+    
+        $question->update($request->all());
+    
+        return redirect()->route('questions.index')
+                        ->with('success','question updated successfully');
+    
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+    
+        return redirect()->route('questions.index')
+                        ->with('success','question deleted successfully');
+    
+    }
+}
